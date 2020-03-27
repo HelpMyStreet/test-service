df5a68c120d9b9be646badaad7fb55690d61dcb9
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-03-27 16:15:22 +0000

    Add repo layer and automapper

---
+++ HelpMyStreet.Mappers/AddressDetailsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HelpMyStreet.Core.Dto;
+using HelpMyStreet.Repo.EntityFramework.Entities;
+//using HelpMyStreet.Repo.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Mappers
+{
+    public class AddressDetailsProfile : Profile
+    {
+        public AddressDetailsProfile()
+        {
+            CreateMap<AddressDetails, AddressDetailsDTO>();
+            CreateMap<AddressDetailsDTO, AddressDetails>();
+        }
+    }
+}
---
+++ HelpMyStreet.Mappers/PostCodeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HelpMyStreet.Core.Dto;
+using HelpMyStreet.Repo.EntityFramework.Entities;
+//using HelpMyStreet.Repo.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpMyStreet.Mappers
+{
+    public class PostCodeProfile : Profile
+    {
+        public PostCodeProfile()
+        {
+            CreateMap<PostCode, PostCodeDTO>()
+                .ForMember(dest => dest.AddressDetails, opt => opt.Ignore());
+            CreateMap<PostCodeDTO, PostCode>()
+                .ForMember(dest => dest.AddressDetails, opt => opt.Ignore());
+        }
+    }
+}
